97fc9aee7acc405486ce417cbeb1121aad23fba8
xia4 <77831217+xia4@users.noreply.github.com>
2026-03-10 16:18:33 +0900

    0310

--- include/arr_data.php
+++ include/arr_data.php
@@ -21,11 +21,19 @@ $P_email2 = "";
 $P_business_no = "123-12-12345";  
 
 // 네비게이션 메뉴명
-$nav_1 = "사용자 투표 랭킹";
-$nav_2 = "심사위원 평가작";
-$nav_3 = "심사위원 되기";
-$nav_4 = "메이커";
+$nav_1 = "메인";
+$nav_2 = "투표하기";
+$nav_3 = "웹랭킹";
+$nav_3_1 = "전체";
+$nav_3_2 = "연도별";
+$nav_3_3 = "월별";
+$nav_4 = "웹랭커";
+$nav_4_1 = "주간 순위정보";
 $nav_5 = "커뮤니티";
+$nav_5_1 = "리소스";
+$nav_5_2 = "Q&A";
+$nav_5_3 = "자유";
+$nav_5_4 = "디지털 인사이트";
 $nav_6 = "서비스 소개";
 
 $nav_policy = "홈페이지 정책";
@@ -34,11 +42,19 @@ $nav_policy_2 = "이용약관";
 $nav_policy_3 = "이메일무단수집거부";
 
 // 네비게이션 하이퍼링크
-$nav_1_href = "../ranking/ranking__list.html";
-$nav_2_href = "../evaluation/evaluation__list.html";
-$nav_3_href = "../judge/judge.html";
-$nav_4_href = "../maker/maker__intro.html";
+$nav_1_href = "../main/main.html";
+$nav_2_href = "../vote/vote_list.html";
+$nav_3_href = "../ranking/ranking_list.html";
+$nav_3_1_href = "../ranking/ranking_list.html";
+$nav_3_2_href = "../ranking/ranking_list.html?type=year";
+$nav_3_3_href = "../ranking/ranking_list.html?type=month";
+$nav_4_href = "../web_ranker/web_ranker_list.html";
+$nav_4_1_href = "../web_ranker/web_ranker_list.html";
 $nav_5_href = "../community/community.html";
-$nav_6_href = "../service/service.html";
+$nav_5_1_href = "../community/community.html?tab=resource";
+$nav_5_2_href = "../community/community.html?tab=qa";
+$nav_5_3_href = "../community/community.html?tab=free";
+$nav_5_4_href = "../community/community.html?tab=insight";
+$nav_6_href = "../service/service_intro.html";
 
 ?>
